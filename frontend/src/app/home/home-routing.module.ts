b15f76cd8b1570189221452d6d5fb41898f5c669
Bubu <80535543+bubu-revelat@users.noreply.github.com>
2024-01-07 18:24:22 -0500

    First Commit

---
+++ frontend/src/app/home/home-routing.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomePage } from './home.page';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomePage,
+    children:[
+      {
+        path:'reservations',
+        loadChildren:()=>
+        import('../pages/reservations/reservations.module').then(
+          m => m.ReservationsPageModule
+        )
+      },
+      {
+        path:'guests',
+        loadChildren:()=>
+        import('../pages/guests/guests.module').then(
+          m => m.GuestsPageModule
+        )
+      },
+      {
+        path:'settings',
+        loadChildren:()=>
+        import('../pages/settings/settings.module').then(
+          m => m.SettingsPageModule
+        )
+      }
+
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class HomePageRoutingModule {}
